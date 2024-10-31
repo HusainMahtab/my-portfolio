42f32c43a87dcacb479463baf30f0cebb4903a03
ahkhan13 <khanahs3351@gmail.com>
2024-10-31 20:37:12 +0530

    responsinve changeses

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -34,7 +34,7 @@ function Home() {
             repeat={Infinity}
           />
             </h1>
-            <p className='text-xl font-semibold md:text-lg'>Over 3 month of hands-on internship experience in full-stack development.Proficient in diverse projects including a E-Commerce Management System,E-Learning Back-End System,School-WebApp,Successfully intergrated payment systems like Razorpay.</p>
+            <p className='text-xl font-semibold md:text-md'>Over 3 month of hands-on internship experience in full-stack development.Proficient in diverse projects including a E-Commerce Management System,E-Learning Back-End System,School-WebApp,Successfully intergrated payment systems like Razorpay.</p>
             <div className='w-full grid md:flex justify-between items-center p-2 gap-3 md:gap-8'>
                 <div className=''>
                     <p className='font-semibold font-sans p-2 text-xl md:text-lg text-center'>Available on</p>
